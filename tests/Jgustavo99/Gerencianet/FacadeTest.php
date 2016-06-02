a8586b5550feff4ff5981f75945f97b539b29653
João Gustavo Balestrin dos Santos <joaogustavo.b@hotmail.com>
2016-06-02 21:59:16 +0000

    Test facade and container instance

--- tests/Jgustavo99/Gerencianet/FacadeTest.php
+++ tests/Jgustavo99/Gerencianet/FacadeTest.php
@@ -2,11 +2,16 @@
 
 namespace Jgustavo99\Gerencianet\Tests;
 
+use Jgustavo99\Gerencianet\Facades\Gerencianet;
 use Jgustavo99\Gerencianet\Tests\AbstractTestCase;
 
 class FacadeTest extends AbstractTestCase
 {
-    public function test_facade_instance () {
+    public function test_container_instance () {
         $this->assertInstanceOf('Gerencianet\Gerencianet', app('Gerencianet'));
+    }
+    
+    public function test_facade_instance () {
+        $this->assertInstanceOf('Gerencianet\Gerencianet', app(\Jgustavo99\Gerencianet\Facades\Gerencianet::class)->getFacadeRoot());
     }
 }
