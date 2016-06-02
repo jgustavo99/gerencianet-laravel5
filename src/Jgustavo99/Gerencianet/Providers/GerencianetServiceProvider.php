e04df4f9b1bcdedee2846b687c4e38d6be5ea507
João Gustavo Balestrin dos Santos <joaogustavo.b@hotmail.com>
2016-06-02 18:44:13 +0000

    Update facade bind name

--- src/Jgustavo99/Gerencianet/Providers/GerencianetServiceProvider.php
+++ src/Jgustavo99/Gerencianet/Providers/GerencianetServiceProvider.php
@@ -9,12 +9,12 @@ class GerencianetServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([__DIR__ . '/../resources/config/gerencianet.php' => config_path('gerencianet.php')]);
+        $this->publishes([__DIR__ . '/../../resources/config/gerencianet.php' => config_path('gerencianet.php')]);
     }
 
     public function register()
     {
-        $this->app->bind('Gerencianet\Gerencianet', function () {
+        $this->app->bind('Gerencianet', function () {
             return new Gerencianet([
                 'client_id' => config('gerencianet.client_id'),
                 'client_secret' => config('gerencianet.client_secret'),
